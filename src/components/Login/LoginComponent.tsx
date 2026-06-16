191947df1f90287ddac87d394be004a36858e6d1
khanh <khanhbarca1410@gmail.com>
2026-06-16 07:39:06 +0700

    reinstall shadcn ui to the project

--- src/components/Login/LoginComponent.tsx
+++ src/components/Login/LoginComponent.tsx
@@ -1,5 +1,11 @@
+import { Button } from '../ui/button';
+
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <div className="flex min-h-svh flex-col items-center justify-center">
+      <Button>Click me</Button>
+    </div>
+  );
 };
 
 export default Login;
